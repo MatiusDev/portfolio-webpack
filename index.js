4574aa16ba7a2f195c88f39fb892789d84ca7a89
TheMatius <thematius.monsalve@gmail.com>
2021-04-26 01:40:30 -0500

    Quitando logs del JS.

--- index.js
+++ index.js
@@ -3,16 +3,9 @@ const btnClose = document.querySelector('#contact__btnClose');
 const frmContact = document.querySelector('.contact__form-container');
 
 btnOpen.addEventListener('click', () => {
-    console.log('opened');
     frmContact.style.left = 0;
 });
 
 btnClose.addEventListener('click', () => {
-    console.log('closed');
     frmContact.style.left = '-2000px';
 });
-
-const container = document.querySelector("#contact");
-const mainContainer = document.querySelector(".main__container");
-
-console.log(container, mainContainer);
